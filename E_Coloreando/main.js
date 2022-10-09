042cf86a2ff235982c1dd8393c95e43a13e4c874
Andrés Viracachá <viracfandres@gmail.com>
2022-10-09 15:55:29 -0500

    se pone el lienzo del tamaño del canva

--- E_Coloreando/main.js
+++ E_Coloreando/main.js
@@ -10,18 +10,29 @@ colorPicker.addEventListener("click",()=>{
 });
 
 let sketch = function(p) {
+    let img;
+    let aspectRatioImg
     p.setup = function(){
-      p.createCanvas(canva.clientWidth,canva.clientHeight);
-      p.background(200);
-      
-    }
-        
+        p.createCanvas(canva.clientWidth,canva.clientHeight);
+        p.background(200);
+        img = p.loadImage("./img/Camion.png")
+    }        
     p.draw = function() {
         p.strokeWeight(10);
         p.stroke(255);
         if (p.mouseIsPressed === true) {
             p.line(p.mouseX, p.mouseY, p.pmouseX, p.pmouseY);
         }
+        if (canva.clientWidth>canva.clientHeight) {
+            p.image(img,canva.clientWidth/2-canva.clientHeight/2,0,canva.clientHeight,canva.clientHeight);
+        }
+        if (canva.clientWidth<canva.clientHeight) {
+            p.image(img, 0, canva.clientHeight/2-canva.clientWidth/2,canva.clientWidth,canva.clientWidth);
+        }
     }
 }
   new p5(sketch, canva);
+  /* 
+  window.onbeforeunload = function() {
+    return "Dude, are you sure you want to leave? Think of the kittens!";
+} */
