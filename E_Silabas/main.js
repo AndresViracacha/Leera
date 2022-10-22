93e7dd444ec3bd9fa0cb71c450d77a1c028e424c
Andrés Viracachá <viracfandres@gmail.com>
2022-10-22 18:27:23 -0500

    maqueto pantalla final de silabas

--- E_Silabas/main.js
+++ E_Silabas/main.js
@@ -3,6 +3,7 @@ const VOCALS = ["a","e","i","o","u"];
 
 const taleOne = ["a","de","la","es","u","na","ar","di","lla","a","de","la","tie","ne","mu","chas","a","mi","gas","a","li","na","es","a","mi","ga","de","a","de","la","a","li","na","es","u","na","ar","di","lla","a","ma","ble","a","li","na","re","co","ge","al","men","dras","a","li","na","com","par","te","las","al","men","dras","con","a","de","la","a","de","la","y","a","li","na","son","bue","nas","a","mi","gas"];
 
+
 const buildSyllabe = () => {
     let syllabe = "";
     syllabe=CONSONANTS[Math.floor(Math.random()*CONSONANTS.length)].toUpperCase()+VOCALS[Math.floor(Math.random()*VOCALS.length)].toLowerCase();
